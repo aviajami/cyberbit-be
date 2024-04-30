e3cf4e832f6c7779bdf7b9aedac08aabd03c0c0d
aviajami <avi.ajami@gmail.com>
2024-04-30 14:58:54 +0300

    initial commit

---
+++ Cyberbit.TaskManager.Server/Cyberbit.TaskManager.Server/Dal/TasksDal.cs
@@ -0,0 +1,97 @@
+﻿using Cyberbit.TaskManager.Server.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cyberbit.TaskManager.Server.Dal
+{
+    public class TasksDal : ITasksDal
+    {
+        private readonly ILogger<TasksDal> _logger;
+        private readonly BackendDbContext _dbContext;
+
+        public TasksDal(ILogger<TasksDal> logger, BackendDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public async Task<IList<Models.Task>> GetAllTask()
+        {
+            _logger.LogInformation($"GetAllTask - Enter");
+            var retValue = await _dbContext.Tasks.AsNoTracking()
+                .Include(t => t.CreatedByUser)
+                .Include(t => t.User).ToListAsync();
+            _logger.LogInformation($"GetAllTask - Exit");
+            return retValue;
+        }
+
+        public async Task<Models.Task> GetTaskById(int id)
+        {
+            _logger.LogInformation($"GetTaskById - Enter");
+            var retValue = await _dbContext.Tasks.AsNoTracking()
+                .Include(t => t.CreatedByUser)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(task => task.Id == id);
+            _logger.LogInformation($"GetTaskById - Exit");
+            return retValue;
+        }
+
+        public async Task<Models.Task> AddTask(Models.Task task)
+        {
+            _logger.LogInformation($"AddTask - Enter");
+            _dbContext.Tasks.Add(task);
+            await _dbContext.SaveChangesAsync();
+            _logger.LogInformation($"AddTask - Exit");
+            return task;
+        }
+
+        public async Task<Models.Task> UpdateTask(Models.Task task)
+        {
+            _logger.LogInformation($"UpdateTask - Enter");
+            var updatedEntity = _dbContext.Tasks.Update(task);
+            await _dbContext.SaveChangesAsync();
+            updatedEntity.State = EntityState.Detached;
+            _logger.LogInformation($"UpdateTask - Exit");
+            return updatedEntity.Entity;
+        }
+
+        public async Task<Models.Task> DeleteTaskById(int id)
+        {
+            _logger.LogInformation($"DeleteTaskById - Enter");
+            var taskToDelete = new Models.Task { Id = id };
+            var deletedEntity = _dbContext.Tasks.Remove(taskToDelete);
+            await _dbContext.SaveChangesAsync();
+            _logger.LogInformation($"DeleteTaskById - Exit");
+            return deletedEntity.Entity;
+        }
+
+        public async Task<IList<Models.Task>> DoneAll(int empolyeeId)
+        {
+            _logger.LogInformation($"DoneAll - Enter");
+            var tasks = _dbContext.Tasks.Where(t => t.UserId == empolyeeId);
+            _dbContext.Tasks.UpdateRange(tasks.Select(t => new Models.Task
+            {
+                Status = Models.Enums.TasksStatus.Done,
+                CreatedByUser = t.CreatedByUser,
+                CreationTime = t.CreationTime,
+                CreatedByUserId = t.CreatedByUserId,
+                Description = t.Description,
+                DueDate = t.DueDate,
+                Id = t.Id,
+                Title = t.Title,
+                User = t.User,
+                UserId = t.UserId
+
+            }));
+            await _dbContext.SaveChangesAsync();
+
+            var retValue = await _dbContext.Tasks.AsNoTracking().Where(t => t.UserId == empolyeeId).ToListAsync();
+                
+            _logger.LogInformation($"DoneAll - Exit");
+            return retValue;
+        }
+    }
+}
---
+++ Cyberbit.TaskManager.Server/Cyberbit.TaskManager.Server/Models/Category.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cyberbit.TaskManager.Server.Models
+{
+    public class Category
+    {
+        [Key, Required]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
